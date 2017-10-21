d594c312314f41deef8c46b4cdd1ec8d075ab076
tutanck <tutanck@gmail.com>
2017-10-21 11:21:29 +0200

    users _needs _keywords indexed

--- functions/index.js
+++ functions/index.js
@@ -11,13 +11,16 @@ const algoliasearch = require('algoliasearch');
 const ALGOLIA_ID = functions.config().algolia.app_id;
 const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
 
-const USERS_INDEX_NAME = "USERS";
-const _KEYWORDS_INDEX_NAME = "_KEYWORDS";
-const _NEEDS_INDEX_NAME = "_NEEDS";
-
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 
 
+//TODO Manage dates and concurrency : must be indempotent and coherent
+
+
+
+
+
+const USERS_INDEX_NAME = "USERS";
 
 //USERS index manager
 exports.syncUser = functions.firestore
@@ -43,7 +46,7 @@ exports.syncUser = functions.firestore
     user.objectID = userID;
     
     //Debug
-    console.log("Will attempt to index:  user/"+userID 
+    console.log("Will attempt to index: user/"+userID 
     ,"\n -->curr=",user ,"\n -->prev=",previous);
     
     // Write to the algolia index
@@ -57,8 +60,86 @@ exports.syncUser = functions.firestore
 
 
 
- 
+const _KEYWORDS_INDEX_NAME = "_KEYWORDS";
 
+//USERS_KEYWODRS index manager
+exports.syncUserKeyword = functions.firestore
+.document(USERS_INDEX_NAME+'/{userID}/'+_KEYWORDS_INDEX_NAME+'/{keywordID}')
+.onWrite(event => {
+
+    //userID
+    const userID =  event.params.userID;
+    
+    //keywordID
+    const keywordID =  event.params.keywordID;
+    
+    // Get an object with the previous document value
+    var previous = event.data.previous?event.data.previous.data():null;
+    
+    // Get an object with the current document value
+    var keyword = event.data.data();
+    
+    if(!keyword.deleted && keyword.active){
+        let keywordStr = keyword.keyword;
+        
+        //Debug
+        console.log("Will attempt to index: user/"+userID+"/_keyword/"+keywordID 
+        ,"\n -->curr=",keyword ,"\n -->prev=",previous);
+        
+        // Write to the algolia index
+        const index = client.initIndex(_KEYWORDS_INDEX_NAME);
+        //return index.saveObject(keywordStr);
+        
+    }else{
+         //Debug
+         console.log("Irrelevant to index: user/"+userID+"/_keyword/"+keywordID 
+         ,"\n -->curr=",keyword ,"\n -->prev=",previous);
+    }
+    
+    return null;
+});
+
+
+
+
+
+const _NEEDS_INDEX_NAME = "_NEEDS";
+
+//USERS_NEEDS index manager
+exports.syncUserNeed = functions.firestore
+.document(USERS_INDEX_NAME+'/{userID}/'+_NEEDS_INDEX_NAME+'/{needID}')
+.onWrite(event => {
+
+    //userID
+    const userID =  event.params.userID;
+    
+    //needID
+    const needID =  event.params.needID;
+    
+    // Get an object with the previous document value
+    var previous = event.data.previous?event.data.previous.data():null;
+    
+    // Get an object with the current document value
+    var need = event.data.data();
+    
+    if(!need.deleted && need.active){
+        
+        //Debug
+        console.log("Will attempt to index: user/"+userID+"/_need/"+needID 
+        ,"\n -->curr=",need ,"\n -->prev=",previous);
+        
+        // Write to the algolia index
+        const index = client.initIndex(_NEEDS_INDEX_NAME);
+        //return index.saveObject(need);
+        
+    }else{
+         //Debug
+         console.log("Irrelevant to index: user/"+userID+"/_need/"+needID 
+         ,"\n -->curr=",need ,"\n -->prev=",previous);
+    }
+    
+    return null;
+});
 
 
 
@@ -69,7 +150,7 @@ exports.syncUser = functions.firestore
 
 // test func
 exports.hello = functions.https.onRequest((request, response) => {
-    const v = 0;
+    const v = 3;
     console.log("my log v="+v);
     response.send("Hello *_* ! v="+v);
 });
