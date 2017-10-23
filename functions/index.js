88c4a95a7ede8eed5494c0b8a9aab808ecb93e58
tutanck <tutanck@gmail.com>
2017-10-23 21:46:31 +0200

    aggregateRatings

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,10 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+// Firestore handler
+var db = admin.firestore();
+
+
 const algoliasearch = require('algoliasearch');
 
 // App ID and API Key are stored in functions config variables
@@ -66,7 +70,7 @@ const _KEYWORDS_INDEX_NAME = "_KEYWORDS";
 exports.syncUserKeyword = functions.firestore
 .document(USERS_INDEX_NAME+'/{userID}/'+_KEYWORDS_INDEX_NAME+'/{keywordID}')
 .onWrite(event => {
-
+    
     //userID
     const userID =  event.params.userID;
     
@@ -91,9 +95,9 @@ exports.syncUserKeyword = functions.firestore
         //return index.saveObject(keywordStr);
         
     }else{
-         //Debug
-         console.log("Irrelevant to index: user/"+userID+"/_keyword/"+keywordID 
-         ,"\n -->curr=",keyword ,"\n -->prev=",previous);
+        //Debug
+        console.log("Irrelevant to index: user/"+userID+"/_keyword/"+keywordID 
+        ,"\n -->curr=",keyword ,"\n -->prev=",previous);
     }
     
     return null;
@@ -109,7 +113,7 @@ const _NEEDS_INDEX_NAME = "_NEEDS";
 exports.syncUserNeed = functions.firestore
 .document(USERS_INDEX_NAME+'/{userID}/'+_NEEDS_INDEX_NAME+'/{needID}')
 .onWrite(event => {
-
+    
     //userID
     const userID =  event.params.userID;
     
@@ -133,12 +137,80 @@ exports.syncUserNeed = functions.firestore
         //return index.saveObject(need);
         
     }else{
-         //Debug
-         console.log("Irrelevant to index: user/"+userID+"/_need/"+needID 
-         ,"\n -->curr=",need ,"\n -->prev=",previous);
+        //Debug
+        console.log("Irrelevant to index: user/"+userID+"/_need/"+needID 
+        ,"\n -->curr=",need ,"\n -->prev=",previous);
     }
     
     return null;
+});
+
+
+
+
+
+
+const _RATINGS_INDEX_NAME = "_RATINGS";
+
+//User's ratings aggregation
+exports.aggregateRatings = functions.firestore
+.document(USERS_INDEX_NAME+'/{userID}/'+_RATINGS_INDEX_NAME+'/{ratingId}')
+.onWrite(event => {
+    
+    //userID
+    const userID =  event.params.userID;
+    
+    //keywordID
+    const ratingId =  event.params.ratingId;
+    
+    // Get value of the newly added rating
+    var ratingVal = event.data.get('rating');
+    
+    // Get a reference to the rated user
+    var userRef = db.collection(USERS_INDEX_NAME).doc(userID);
+    
+    // Get a reference to the rated user ratings collection
+    var userRatingsRef = userRef.collection(_RATINGS_INDEX_NAME) ;
+    
+    //debug
+    console.log(1,'aggregateRatings','userID='+userID
+    ,'ratingId='+ratingId,'ratingVal='+ratingVal);
+    
+    // Update aggregations in a transaction
+    return db.runTransaction(transaction => {
+        return transaction.get(userRatingsRef).then(snapshot => {
+            let nbVoters = 0;
+            let ratingsSum = 0;
+            
+            snapshot.forEach(doc => {
+                ratingsSum += doc.get('rating');
+                nbVoters++;      
+                
+                //debug
+                console.log(2,'aggregateRatings','userID='+userID
+                ,'ratingId='+ratingId,'ratingVal='+ratingVal
+                ,'ratingsSum='+ratingsSum,'nbVoters='+nbVoters
+                ,doc.id, '=>', doc.data());
+            });
+            
+            let avgRating = nbVoters == 0 ? 0 : ratingsSum / nbVoters;
+            
+            //debug
+            console.log(3,'aggregateRatings','userID='+userID
+            ,'ratingId='+ratingId,'ratingVal='+ratingVal
+            ,"avgRating="+avgRating,"nbVoters="+nbVoters);
+            
+            // Update user rating infos
+            return transaction.update(userRef, {
+                avgRating: avgRating,
+                nbVoters: nbVoters
+            });
+            
+        })
+        .catch(err => {
+            console.log('aggregateRatings','userRatingsRef : Error getting user{'+userID+'} ratings', err);
+        });       
+    });
 });
 
 
@@ -150,7 +222,7 @@ exports.syncUserNeed = functions.firestore
 
 // test func
 exports.hello = functions.https.onRequest((request, response) => {
-    const v = 3;
+    const v = 5;
     console.log("my log v="+v);
     response.send("Hello *_* ! v="+v);
 });
