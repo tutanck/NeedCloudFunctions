6a969408baeff715c6311e643d6075bddd9caa2c
tutanck <tutanck@gmail.com>
2017-11-21 21:02:16 +0100

    function 3.0

--- functions/index.js
+++ functions/index.js
@@ -21,6 +21,7 @@ const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 //TODO Manage dates and concurrency : must be indempotent and coherent
 
 
+const coll = require('./coll');
 
 
 
@@ -38,20 +39,27 @@ exports.syncUser = functions.firestore
     var previous = event.data.previous?event.data.previous.data():null;
     
     // Get an object with the current document value
-    var user = event.data.data();
+    var user = event.data.data();    
+    
+    //remove from index offline users
+    if(user.availability===-1) {
+        //todo remove from index
+        return null;
+    }
     
     //Document minimization 
     delete user.resume;
     delete user.lastRead;
     delete user.tariff;
-    delete user.type;
+    delete user.instanceIDToken;
+    delete user.type;   
+    
     
     // Add an "objectID" field which Algolia requires
     user.objectID = userID;
     
     //Debug
-    console.log("Will attempt to index: user/"+userID 
-    ,"\n -->curr=",user ,"\n -->prev=",previous);
+    console.log("Will attempt to re-index: user/"+userID ,"\n -->curr=",user ,"\n -->prev=",previous);
     
     // Write to the algolia index
     const index = client.initIndex(USERS_INDEX_NAME);
@@ -64,10 +72,11 @@ exports.syncUser = functions.firestore
 
 
 
+
 const _KEYWORDS_INDEX_NAME = "_KEYWORDS";
 
 //USERS_KEYWODRS index manager
-exports.syncUserKeyword = functions.firestore
+exports.syncUserKeywords = functions.firestore
 .document(USERS_INDEX_NAME+'/{userID}/'+_KEYWORDS_INDEX_NAME+'/{keywordID}')
 .onWrite(event => {
     
@@ -83,27 +92,41 @@ exports.syncUserKeyword = functions.firestore
     // Get an object with the current document value
     var keyword = event.data.data();
     
-    if(!keyword.deleted && keyword.active){
-        let keywordStr = keyword.keyword;
+    // Get a reference to the user that own the keyword
+    var userRef = db.collection(USERS_INDEX_NAME).doc(userID);
+    
+    // Get a reference to the user keyword's collection
+    var userKeywordsRef = userRef.collection(_KEYWORDS_INDEX_NAME);
+    
+    //Get all user keywords for indexing
+    return userKeywordsRef.get().then(snapshot => {
+        const kTab = [];
+        
+        snapshot.forEach(doc => {
+            //console.log(2,doc.id, '=>', doc.data());//debug
+            
+            if(!doc.get('deleted') && doc.get('active'))
+                kTab.push(doc.get('keyword'));
+            
+        });    
         
         //Debug
-        console.log("Will attempt to index: user/"+userID+"/_keyword/"+keywordID 
-        ,"\n -->curr=",keyword ,"\n -->prev=",previous);
+        console.log(2,"Will attempt to re-index:",userKeywordsRef.path,"\n -->kTab=",kTab);
         
         // Write to the algolia index
         const index = client.initIndex(_KEYWORDS_INDEX_NAME);
-        //return index.saveObject(keywordStr);
+        //return index.saveObject(kTab);
         
-    }else{
-        //Debug
-        console.log("Irrelevant to index: user/"+userID+"/_keyword/"+keywordID 
-        ,"\n -->curr=",keyword ,"\n -->prev=",previous);
-        
-        //todo : remove from index
-    }
+    })
+    .catch(err => {
+        console.log(2,'syncUserKeywords','userRef : Error getting document','userID='+userID, err);
+    });
     
     return null;
 });
+
+
+
 
 
 
@@ -128,10 +151,26 @@ exports.syncUserNeed = functions.firestore
     // Get an object with the current document value
     var need = event.data.data();
     
+    // Get a reference to the user that own the need
+    var userRef = db.collection(USERS_INDEX_NAME).doc(userID);
+    
+    // Get a reference to the user's need 
+    var userNeedRef = userRef.collection(_NEEDS_INDEX_NAME).doc(needID);
+    
+    // Get a reference to the need's applicants collection
+    var userNeedApplicantsRef = userNeedRef.collection(_APPLICANTS_INDEX_NAME);
+    
+    coll.deleteCollection(db,userNeedApplicantsRef,25).then(function(result) {
+        console.log(2,userNeedApplicantsRef.path, "has been successfully deleted."); 
+    }, function(err) {
+        console.log(2,"an error occured while deleting",userNeedApplicantsRef.path,err); 
+    });
+    
+    
     if(!need.deleted && need.active){
         
         //Debug
-        console.log("Will attempt to index: user/"+userID+"/_need/"+needID 
+        console.log(1,"Will attempt to re-index:", userNeedRef.path
         ,"\n -->curr=",need ,"\n -->prev=",previous);
         
         // Write to the algolia index
@@ -140,7 +179,7 @@ exports.syncUserNeed = functions.firestore
         
     }else{
         //Debug
-        console.log("Irrelevant to index: user/"+userID+"/_need/"+needID 
+        console.log(1,"Irrelevant to index:",userNeedRef.path
         ,"\n -->curr=",need ,"\n -->prev=",previous);
         
         //todo : remove from index
@@ -152,6 +191,14 @@ exports.syncUserNeed = functions.firestore
 
 
 
+
+
+
+
+
+
+
+/*DB TRIGGERS*/ 
 
 
 const _RATINGS_INDEX_NAME = "_RATINGS";
@@ -227,7 +274,7 @@ const _APPLICANTS_INDEX_NAME = "_APPLICANTS";
 //User's ratings aggregation
 exports.manageNeedApplications = functions.firestore
 .document(USERS_INDEX_NAME+'/{userID}/'+_NEEDS_INDEX_NAME+'/{needID}/'+_APPLICANTS_INDEX_NAME+'/{applicantID}')
-.onWrite(event => {
+.onCreate(event => {
     
     //userID
     const userID =  event.params.userID;
@@ -309,25 +356,22 @@ exports.manageNeedApplications = functions.firestore
 
 
 
-
-
-
 const MESSAGES_INDEX_NAME = "MESSAGES";
 
 //User's ratings aggregation
 exports.messagesNotifications = functions.firestore
 .document(MESSAGES_INDEX_NAME+'/{messageID}')
 .onCreate(event => {
-
+    
     //messageID
     const messageID =  event.params.messageID;
-
+    
     //fromID
     const fromID =  event.data.get('from');
-
+    
     //toID
     const toID =  event.data.get('to');
-
+    
     //message
     const message =  event.data.get('message');
     
@@ -346,6 +390,7 @@ exports.messagesNotifications = functions.firestore
             console.log(2,'Document data:', doc.data());
             
             let registrationToken = doc.get('instanceIDToken');
+            let userAvail =  doc.get('availability');
             
             var payload = {
                 notification: {
@@ -364,13 +409,17 @@ exports.messagesNotifications = functions.firestore
                 timeToLive: 60 //TODO(uncomment in prod mod)    * 60 * 24  //24h bf expiration
             };
             
-            admin.messaging().sendToDevice(registrationToken, payload, options)
-            .then(function(response) {
-                console.log(3,"Successfully sent message To {"+toID+"("+registrationToken+")} : response=", response);
-            })
-            .catch(function(error) {
-                console.log(3,"Error sending message To {"+toID+"("+registrationToken+")} : error=", error);
-            });
+            if(userAvail===1){//user wants notifications only if he is available
+                admin.messaging().sendToDevice(registrationToken, payload, options)
+                .then(function(response) {
+                    console.log(3,"Successfully sent message To {"+toID+"("+registrationToken+")} : response=", response);
+                })
+                .catch(function(error) {
+                    console.log(3,"Error sending message To {"+toID+"("+registrationToken+")} : error=", error);
+                });
+            }else{
+                console.log(2,'User is not available at the moment... message not sent.');
+            }
             
         }
     })
@@ -384,11 +433,13 @@ exports.messagesNotifications = functions.firestore
 
 
 
+/*TOOLS*/
+
 
 
 // test func
 exports.hello = functions.https.onRequest((request, response) => {
-    const v = 5;
+    const v =0;
     console.log("my log v="+v);
     response.send("Hello *_* ! v="+v);
 });
